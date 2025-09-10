34ca75c91de2f96ba82bb3bbe600ea07a32ab8cd
Aditya Jangid <116833294+akshay5568@users.noreply.github.com>
2025-09-10 23:26:46 +0530

    Fixing code in signup component

--- Frontend/twitter-clone/src/components/MainPage.jsx
+++ Frontend/twitter-clone/src/components/MainPage.jsx
@@ -97,7 +97,7 @@ function MainPage() {
         </div>
 
         {Toggle ? (
-          <div className="absolute bg-black p-3 rounded-xl text-sm left-2 border-1 border-gray-800">
+          <div className="absolute bg-black p-3 rounded-xl text-sm left-2 border-1 border-gray-800">  
             <div className="">
               <button onClick={logoutMobile}>Logout</button>
             </div>
@@ -209,7 +209,7 @@ function MainPage() {
           <button
             disabled
             type="button"
-            className="py-2.5 px-5 me-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
+            className="py-2.5 px-5 me-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100         hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
           >
             <svg
               aria-hidden="true"
